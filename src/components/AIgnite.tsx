5f773b478d8ce3e10de33dc33579c01b3a12df25
alenavarroxp <alejandropvp13@gmail.com>
2024-10-07 10:44:54 +0200

    principal page design

--- src/components/AIgnite.tsx
+++ src/components/AIgnite.tsx
@@ -1,10 +1,10 @@
 export default function AIgnite() {
   return (
-    <div className="flex justify-center items-center mb-2">
-      <h1 className="text-6xl font-bold  bg-gradient-to-r py-2.5 from-blue-500 to-purple-600 bg-clip-text text-transparent">
+    <div className="flex justify-center items-center gap-0.5">
+      <h1 className="text-6xl font-bold  bg-gradient-to-r py-2.5 from-blue-500 to-purple-600 bg-clip-text text-transparent tracking-tighter">
         AI
       </h1>
-      <h1 className="text-6xl font-bold bg-gradient-to-l py-2.5 from-slate-100 to-purple-600 bg-clip-text text-transparent">
+      <h1 className="text-6xl font-bold bg-gradient-to-l py-2.5 from-slate-100 to-purple-600 bg-clip-text text-transparent tracking-tight">
         gnite
       </h1>
     </div>
